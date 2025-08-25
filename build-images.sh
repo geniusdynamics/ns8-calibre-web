8daf899851a602c5b0a9319d55c54c46e2ed53ad
Martin Bhuong <martin@genius.ke>
2025-08-25 09:05:06 +0300

    Update to 0.6.25

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="calibre-web"
-calibre_version="0.6.24"
+calibre_version="0.6.25"
 
 # Create a new empty container image
 container=$(buildah from scratch)
